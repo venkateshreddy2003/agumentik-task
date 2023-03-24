055bd81962a2fb285584cc7696324d7049ebcbda
venkateshreddy2003 <20bq1a05j4@vvit.net>
2023-03-24 18:25:44 +0530

    commit

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,7 +1,9 @@
 const mongoose = require("mongoose");
 
-const connect = async ()=>{
-    return await mongoose.connect("mongodb+srv://nitesh:samaniya@cluster0.fsv9b7i.mongodb.net/Agumentic");
-}
+const connect = async () => {
+  return await mongoose.connect(
+    "mongodb+srv://venkatesh:N1mvHO3FOpxgiAXz@cluster0.mmjfc3k.mongodb.net/?retryWrites=true&w=majority"
+  );
+};
 
 module.exports = connect;
