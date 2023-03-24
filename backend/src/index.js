6847f3fe3e708ce7332abcdaf98f927a721d0026
venkateshreddy2003 <20bq1a05j4@vvit.net>
2023-03-24 18:14:17 +0530

    commit

--- backend/src/index.js
+++ backend/src/index.js
@@ -6,25 +6,25 @@ const cors = require("cors");
 const bodyparser = require("body-parser");
 
 const connect = require("./config/db");
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 5000;
 
-const BannerRoute = require("./route/banner.route")
+const BannerRoute = require("./route/banner.route");
 
 const app = express();
 app.use(express.json());
 app.use(cors());
-app.use(bodyparser.urlencoded({extended: false}));
+app.use(bodyparser.urlencoded({ extended: false }));
 app.use(bodyparser.json());
 
 mongoose.set("strictQuery", false);
 
 app.use("/banner", BannerRoute);
 
-app.get("/", (req,res)=> {
-    res.send("Agumentic Task");
-})
+app.get("/", (req, res) => {
+  res.send("Agumentic Task");
+});
 
-app.listen(PORT, async()=>{
-    connect();
-    console.log(`listening at port http://localhost:${PORT}`);
-})
+app.listen(PORT, async () => {
+  connect();
+  console.log(`listening at port http://localhost:${PORT}`);
+});
